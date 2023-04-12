c0051133c40ea21d953f0af69868bfca891f754a
missfall111111 <3203565916@qq.com>
2023-04-12 23:02:10 +0800

    a lot of things

--- C++_learning/nju-learn/hw1/使用迭代器编写二分查找算法.cpp
+++ C++_learning/nju-learn/hw1/使用迭代器编写二分查找算法.cpp
@@ -1,1 +1,34 @@
+//用迭代器编写二分查找算法
 #include<iostream>
+#include<vector>
+using std::cin;
+using std::cout;
+using std::vector;
+int main()
+{
+    vector<int> arr={1,2,3,4,5,6,7,8,9};
+
+    vector<int>::iterator low=arr.begin();
+    vector<int>::iterator high=arr.end();
+    int key;
+    cin>>key;
+    while(low<=high)
+    {
+        vector<int>::iterator mid=low+(high-low)/2;
+        if(*mid>key)
+        {
+            high=mid-1;
+        }
+        else if(*mid<key)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            cout<<"The "<<key<<"at the "<<mid-arr.begin()+1;
+            return 0;
+        }
+    }
+        cout<<"Not found!";
+        return 0;
+}
